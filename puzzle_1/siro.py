4061464390734ef4840f734da6cd72dd19210eca
dubi du <100496683@alumnos.uc3m.es>
2024-01-27 19:50:55 +0100

    Solución de siro

--- puzzle_1/siro.py
+++ puzzle_1/siro.py
@@ -1,1 +1,11 @@
+# Datos iniciales. No hay validación de input
+p = [3, 10, 5]
+r = [2, 4, 4]
 
+n = r[0]
+sumando = p[0]
+for i in range(1, len(p)):
+    while (n % p[i] != r[i]):
+        n = n + sumando
+    sumando = sumando * p[i]
+print("para las tuplas p = ", p, "r = ", r, ", n sería ", n)
