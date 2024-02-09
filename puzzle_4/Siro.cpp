5146d9b4481e4fe6748829dc0b761526a3385f38
dubi du <100496683@alumnos.uc3m.es>
2024-02-09 13:15:28 +0100

    puzzle 4

---
+++ puzzle_4/Siro.cpp
@@ -0,0 +1,14 @@
+ #include <iostream>
+#include <vector>
+using namespace std;
+
+
+int main(){
+  int num;
+  do {
+    int num1, num2, num3;
+    cin >> num1 >> num2 >> num3;
+    
+  } while(num1 or num2 or num3);
+  return 0;
+}  
