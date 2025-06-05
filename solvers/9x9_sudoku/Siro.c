ca0ba58dc756b574a37ca0b65ecdf81c118164db
dubi du <100496683@alumnos.uc3m.es>
2025-06-05 14:26:44 +0200

    camino consistencia

--- solvers/9x9_sudoku/Siro.c
+++ solvers/9x9_sudoku/Siro.c
@@ -20,7 +20,6 @@ void poner_a_cero(struct board *tablero) {
 }
 
 void myprint(struct board tablero) {
-  printf("\n");
   int cero = 0;
   for (int i = 0; i < 81; i++) {
     if (tablero.real[i] == 0) {
@@ -126,7 +125,6 @@ void poblar(struct board *tablero, char *file) {
 
 // Escribe un valor en una casilla y lo imposibiliza en las de alrededor
 void marcar(struct board *tablero, int pos, int valor) {
-  printf("%d ", pos);
   int fila, col, cuad, sum_fila, sum_col, sum_cuad, pos_fila, pos_col, pos_cuad;
   fila = pos / 9;
   col = pos % 9;
@@ -245,9 +243,173 @@ int unico_en_set(struct board *tablero) {
   return 0;
 }
 
-int main() {
-  struct board tablero;
-  poblar(&tablero, "sudoku2.txt");
-  unico_en_set(&tablero);
-  myprint(tablero);
+int serializar_posibilidades(struct board *tablero, int pos) {
+  if (tablero->real[pos] != 0) {
+    return 0;
+  }
+  int output = 0, pow = 1;
+  for (int i = 0; i < 9; i++) {
+    output += tablero->posibilidades[pos][i] * pow;
+    pow = pow * 2;
+  }
+  return output;
+}
+void deserializar_posibilidades(int *output, int num) {
+  int pow = 2;
+  for (int i = 0; i < 9; i++) {
+    output[i] = (num % pow == pow / 2);
+    num -= num % pow;
+    pow = pow * 2;
+  }
+}
+
+int naked_subset(struct board *tablero) {
+  int patrones_cuad[9], cuenta_cuad[9], composicion_cuad[9], pos_cuad, num_cuad,
+      index_cuad, found_cuad;
+  int patrones_fila[9], cuenta_fila[9], composicion_fila[9], pos_fila, num_fila,
+      index_fila, found_fila;
+  int patrones_col[9], cuenta_col[9], composicion_col[9], pos_col, num_col,
+      index_col, found_col;
+  int cambio = 0, pow, aux[9];
+  for (int set = 0; set < 9; set++) {
+    index_cuad = 1;
+    index_fila = 1;
+    index_col = 1;
+    for (int i = 0; i < 9; i++) {
+      patrones_cuad[i] = 0;
+      patrones_fila[i] = 0;
+      patrones_col[i] = 0;
+      cuenta_cuad[i] = 0;
+      cuenta_fila[i] = 0;
+      cuenta_col[i] = 0;
+      composicion_cuad[i] = 0;
+      composicion_fila[i] = 0;
+      composicion_col[i] = 0;
+    }
+
+    for (int i = 0; i < 9; i++) {
+      pos_cuad = cuad_it(set, i);
+      pos_fila = 9 * set + i;
+      pos_col = set + 9 * i;
+      num_cuad = serializar_posibilidades(tablero, pos_cuad);
+      num_fila = serializar_posibilidades(tablero, pos_fila);
+      num_col = serializar_posibilidades(tablero, pos_col);
+      found_cuad = 0;
+      found_fila = 0;
+      found_col = 0;
+      for (int j = 0; j < 9; j++) {
+        if ((patrones_cuad[j] == num_cuad) && (found_cuad == 0)) {
+          found_cuad = 1;
+          cuenta_cuad[j]++;
+        }
+        if ((patrones_fila[j] == num_fila) && (found_fila == 0)) {
+          found_fila = 1;
+          cuenta_fila[j]++;
+        }
+        if ((patrones_col[j] == num_col) && (found_col == 0)) {
+          found_col = 1;
+          cuenta_col[j]++;
+        }
+      }
+
+      if (found_cuad == 0) {
+        patrones_cuad[index_cuad] = num_cuad;
+        cuenta_cuad[index_cuad]++;
+        for (int j = 0; j < 9; j++) {
+          composicion_cuad[index_cuad] += tablero->posibilidades[pos_cuad][j];
+        }
+        index_cuad++;
+      }
+      if (found_fila == 0) {
+        patrones_fila[index_fila] = num_fila;
+        cuenta_fila[index_fila]++;
+        for (int j = 0; j < 9; j++) {
+          composicion_fila[index_fila] += tablero->posibilidades[pos_fila][j];
+        }
+        index_fila++;
+      }
+      if (found_col == 0) {
+        patrones_col[index_col] = num_col;
+        cuenta_col[index_col]++;
+        for (int j = 0; j < 9; j++) {
+          composicion_col[index_col] += tablero->posibilidades[pos_col][j];
+        }
+        index_col++;
+      }
+    }
+
+    for (int i = 0; i < index_cuad; i++) {
+      if (cuenta_cuad[i] == composicion_cuad[i]) {
+        deserializar_posibilidades(aux, patrones_cuad[i]);
+        for (int j = 0; j < 9; j++) {
+          pos_cuad = cuad_it(set, j);
+          num_cuad = serializar_posibilidades(tablero, pos_cuad);
+          if (num_cuad != patrones_cuad[i]) {
+            for (int k = 0; k < 9; k++) {
+              if (tablero->posibilidades[pos_cuad][k] == aux[k]) {
+                cambio = 1;
+                tablero->posibilidades[pos_cuad][k] -= aux[k];
+              }
+            }
+          }
+        }
+      }
+    }
+    for (int i = 0; i < index_fila; i++) {
+      if (cuenta_fila[i] == composicion_fila[i]) {
+        deserializar_posibilidades(aux, patrones_fila[i]);
+        for (int j = 0; j < 9; j++) {
+          pos_fila = 9 * set + j;
+          num_fila = serializar_posibilidades(tablero, pos_fila);
+          if (num_fila != patrones_fila[i]) {
+            for (int k = 0; k < 9; k++) {
+              if (tablero->posibilidades[pos_fila][k] == aux[k]) {
+                cambio = 1;
+                tablero->posibilidades[pos_fila][k] -= aux[k];
+              }
+            }
+          }
+        }
+      }
+    }
+    for (int i = 0; i < index_col; i++) {
+      if (cuenta_col[i] == composicion_col[i]) {
+        deserializar_posibilidades(aux, patrones_col[i]);
+        for (int j = 0; j < 9; j++) {
+          pos_col = set + 9 * j;
+          num_col = serializar_posibilidades(tablero, pos_col);
+          if (num_col != patrones_col[i]) {
+            for (int k = 0; k < 9; k++) {
+              if (tablero->posibilidades[pos_col][k] == aux[k]) {
+                cambio = 1;
+                tablero->posibilidades[pos_col][k] -= aux[k];
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+  if (cambio == 1) {
+    return 1;
+  }
+  return 0;
+}
+
+void solve(struct board *tablero) {
+  int cambio = 1;
+  while (cambio != 0) {
+    cambio = 0;
+    cambio += unico_en_set(tablero);
+    cambio += naked_subset(tablero);
+  }
+}
+
+int main(int argc, char *argv[]) {
+  if (argc == 2) {
+    struct board tablero;
+    poblar(&tablero, argv[1]);
+    solve(&tablero);
+    myprint(tablero);
+  }
 }
